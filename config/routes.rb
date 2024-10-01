cf1635a0ba5c00982ba4c37222fded0589bd29b5
Marine <marine.monnier-baron@proton.me>
2024-10-01 18:19:05 +0200

    remove all

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-DiscourseOdooSync::Engine.routes.draw do
-    get "/events/:post_id/invitees" => "invitees#index"
-    post "/events/:post_id/invitees" => "invitees#create"
-  end
-  
-  Discourse::Application.routes.draw { mount ::DiscourseOdooSync::Engine, at: "discourse-odoo-sync" }
--- plugin.rb
+++ plugin.rb
@@ -2,12 +2,5 @@
 # about: Synchronisation between Discourse and Odoo
 # version: 0.1
 # authors: Marine Monnier-Baron
-# url: https://gitlab.com/coopaname/discourse-odoo-sync
+# url: https://github.com/MarineBaron/discourse-odoo-sync
 
-enabled_site_setting :discourse_odoo_sync_enabled
-
-module ::DiscourseOdooSync
-    PLUGIN_NAME = "discourse-odoo-sync"
-  end
-
-require_relative "lib/discourse_odoo_sync/engine"
